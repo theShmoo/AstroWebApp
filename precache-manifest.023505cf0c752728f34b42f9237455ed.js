75b7e90423d436c9a632c54302d245548b3efd70
David Pfahler <pfahler.david@gmail.com>
2020-04-17 13:34:02 +0200

    Updates

---
+++ precache-manifest.023505cf0c752728f34b42f9237455ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99891978cd1e7a1f755c17c279a37e0",
+    "url": "/AstroWebApp/index.html"
+  },
+  {
+    "revision": "288b079f902c029ed2f0",
+    "url": "/AstroWebApp/static/js/2.5570da79.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AstroWebApp/static/js/2.5570da79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc73eba9749479f1b486",
+    "url": "/AstroWebApp/static/js/main.101a3f3c.chunk.js"
+  },
+  {
+    "revision": "fb8ebb39fce2a64b9527",
+    "url": "/AstroWebApp/static/js/runtime-main.87d1a60d.js"
+  }
+]);
